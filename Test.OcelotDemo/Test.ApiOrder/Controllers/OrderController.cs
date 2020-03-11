b2f44210bb5b3a27d0d7d5ce1eef53fdf35a30dd
zwc <946061526@qq.com>
2020-03-11 16:56:35 +0800

    new

---
+++ Test.OcelotDemo/Test.ApiOrder/Controllers/OrderController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.ApiOrder.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        [Route("AddOrder")]
+        [HttpPost]
+        public int AddOrder([FromBody]AddOrder request)
+        {
+            return 1;
+        }
+
+        [Route("GetOrder")]
+        [HttpGet]
+        public OrderInfo GetOrder(string id, string name)
+        {
+            return new OrderInfo() { id = id, name = name };
+        }
+
+        [Route("GetOrderList")]
+        [HttpGet]
+        public List<OrderInfo> GetOrderList(string id, string name)
+        {
+            return new List<OrderInfo>() { new OrderInfo() { id = id, name = name } };
+        }
+
+        [Route("GetOrderList2")]
+        [HttpPost]
+        public List<OrderInfo> GetOrderList2([FromBody]OrderInfo OrderInfo)
+        {
+            return new List<OrderInfo>() { OrderInfo };
+        }
+    }
+
+    public class AddOrder
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class OrderInfo
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ Test.OcelotDemo/Test.ApiUser/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.ApiUser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [Route("AddUser")]
+        [HttpPost]
+        public int AddUser([FromBody]AddUser request)
+        {
+            return 1;
+        }
+
+        [Route("GetUser")]
+        [HttpGet]
+        public UserInfo GetUser(string name, string pwd)
+        {
+            return new UserInfo() { Name = name, Pwd = pwd };
+        }
+
+        [Route("GetUserList")]
+        [HttpGet]
+        public List<UserInfo> GetUserList(string name, string pwd)
+        {
+            return new List<UserInfo>() { new UserInfo() { Name = name, Pwd = pwd } };
+        }
+
+        [Route("GetUserList2")]
+        [HttpPost]
+        public List<UserInfo> GetUserList2([FromBody]UserInfo userInfo)
+        {
+            return new List<UserInfo>() { userInfo };
+        }
+    }
+
+    public class AddUser
+    {
+        public string Name { get; set; }
+        public string Pwd { get; set; }
+    }
+    public class UserInfo
+    {
+        public string Name { get; set; }
+        public string Pwd { get; set; }
+    }
+}
